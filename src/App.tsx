9fc67cef124b6537c4ff00cb12d805d42258eaaf
jitu16 <jitumstock@gmail.com>
2026-01-08 21:39:20 -0600

    First push!

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// src/App.tsx
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <h1>Math Multiverse: Systems Online</h1>
+    </div>
+  );
+}
+
+export default App;
